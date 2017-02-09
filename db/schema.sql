be5bae76652240bc1184ffe3c911c5109992b04d
rxtATX <rachel.thiim@gmail.com>
2017-02-08 23:10:00 -0600

    Created initial files structure and database.

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE IF NOT EXISTS burgers (
+id INT AUTO_INCREMENT NOT NULL
+, burger_name VARCHAR(30) NOT NULL
+, devoured BOOLEAN NOT NULL
+, date TIMESTAMP
+, PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Mushroom and Swiss", true)
+, ("Texan", false)
+, ("Jalapeno and Cheddar", false);
