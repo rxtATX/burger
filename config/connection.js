fd82c76ab2ecb8f7f3162595acb0d1f3ebcf0200
rxtATX <rachel.thiim@gmail.com>
2017-02-10 15:12:53 -0600

    still experimenting with heroku. removing ability to connect on local host.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql = require("mysql");
-var passroute = require("../keys.js");
-var passcode = passroute.passcode;
+// var passroute = require("../keys.js");
+// var passcode = passroute.passcode;
 var PASSWORD;
 
 var connection = mysql.createConnection({
 	host: "localhost",
 	port: 3306,
 	user: "root",
-	password: (PASSWORD || passcode),
+	password: (PASSWORD),
 	database: "burgers_db"
 });
 
