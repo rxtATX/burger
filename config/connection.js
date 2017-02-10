3a16da3bbf627a89a74cab09a9716f21280573a5
rxtATX <rachel.thiim@gmail.com>
2017-02-10 15:11:09 -0600

    Fixing up heroku

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,15 @@
 var mysql = require("mysql");
-var passcode = require("../keys.js");
-var connection = passcode.connection;
+var passroute = require("../keys.js");
+var passcode = passroute.passcode;
+var PASSWORD;
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: (PASSWORD || passcode),
+	database: "burgers_db"
+});
 
 connection.connect(function(err) {
     if (err) throw err;
