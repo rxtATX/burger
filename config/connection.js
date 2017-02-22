ea4f0f85cce3aa93926e04a589afea6ac46312a5
rxtATX <rachel.thiim@gmail.com>
2017-02-21 18:56:35 -0600

    Heroku uploading

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,10 @@ var mysql = require("mysql");
 var passcode = require("../keys.js");
 var connection = passcode.connection;
 
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWS_URL);
+};
+
 connection.connect(function(err) {
     if (err) throw err;
     console.log("Connection thread: " + connection.threadId);
